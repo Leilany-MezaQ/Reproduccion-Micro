5aa7031d96da1a55df348a0f2238dc8427e492d3
poblanafranco <poblanafranco@gmail.com>
2026-03-22 22:35:22 -0600

    feat: video player con guardado de progreso

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,39 +1,3 @@
-// import { useState } from 'react'
-// import { PlayIcon } from './layout/icons'
-
-// interface VideoPlayerProps {
-//   src?: string
-//   poster?: string
-// }
-
-// export const VideoPlayer = ({ src, poster }: VideoPlayerProps) => {
-//   const [playing, setPlaying] = useState(false)
-
-//   return (
-//     <div className="video-player">
-//       {!playing ? (
-//         <div
-//           className="video-placeholder"
-//           style={poster ? { backgroundImage: `url(${poster})` } : {}}
-//           onClick={() => src && setPlaying(true)}
-//         >
-//           <button className="play-btn">
-//             <PlayIcon />
-//           </button>
-//         </div>
-//       ) : (
-//         <video
-//           className="video-element"
-//           src={src}
-//           autoPlay
-//           controls
-//           poster={poster}
-//         />
-//       )}
-//     </div>
-//   )
-// }
-
 import { useEffect, useRef } from 'react'
 import { api } from '@/lib/axiosConfig'
 
